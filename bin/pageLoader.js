d2e5a64361ec3ff31a4e3cefd893c28a15527c8d
thanatos <seniaslams@gmail.com>
2022-08-09 15:42:43 +0500

    Add description

--- bin/pageLoader.js
+++ bin/pageLoader.js
@@ -6,8 +6,8 @@ const program = new Command();
 
 program
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format', 'stylish');
+  .description('Page loader utility')
+  .option('-o, --output [dir]', 'output dir', '/home/user/current-dir');
 //   .arguments('<filepath1>')
 //   .arguments('<filepath2>')
 //   .action((filepath1, filepath2) => {
