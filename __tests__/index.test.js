5aa004c6a0b723884faea22f30aec8519d76dbd1
thanatos <seniaslams@gmail.com>
2022-08-11 15:06:03 +0500

    Update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,15 +2,15 @@
 // import axios from 'axios';
 import os from 'os';
 import nock from 'nock';
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 import path from 'path';
 import fsp from 'fs/promises';
 import downloadPage from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 const tmpFilePath = path.join(os.tmpdir());
-const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
+// const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
 // const readFile = (dir, file) => fsp.readFile(path.join(dir, file));
 // const getPath = (dirname, filename) => path.join(dirname, filename);
 
