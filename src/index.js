047643dae1c43d6b665e428c769b838d8a91ed44
Arseny <seniaslams@gmail.com>
2022-08-15 16:39:08 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import fsp from 'fs/promises';
 import path from 'path';
 
-const downloadPage = (filePath, link) => {
-  const fileName = link.replace(/htt(p|ps):\/\//, '').replace(/\W/g, '-');
+const downloadPage = (filePath, url) => {
+  const fileName = url.replace(/htt(p|ps):\/\//, '').replace(/\W/g, '-');
   const resultPath = path.join(filePath, fileName);
-  return axios.get(link)
+  return axios.get(url)
     .then(({ data }) => fsp.writeFile(`${resultPath}.html`, data))
     .catch((e) => { throw new Error(e); });
 };
