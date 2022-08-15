064c6812c34d5aa1d8bf0e370ce36b201923112e
Arseny <seniaslams@gmail.com>
2022-08-15 14:03:56 +0500

    Update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,27 +1,23 @@
 /* eslint-disable no-undef */
-// import axios from 'axios';
 import os from 'os';
 import nock from 'nock';
-// import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url';
 import path from 'path';
 import fsp from 'fs/promises';
 import downloadPage from '../src/index.js';
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const tmpFilePath = path.join(os.tmpdir());
-// const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
-// const readFile = (dir, file) => fsp.readFile(path.join(dir, file));
-// const getPath = (dirname, filename) => path.join(dirname, filename);
+const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
 
-// let data;
-const actualData = 'data';
+let data;
 
 nock.disableNetConnect();
 
-// beforeAll(async () => {
-//   data = await fsp.readFile(getFixturePath('ru-hexlet-io-courses.html'));
-// });
+beforeAll(async () => {
+  data = await fsp.readFile(path.join(getFixturePath('ru-hexlet-io-courses_files'), 'ru-hexlet-io-courses.html'), 'utf-8');
+});
 
 afterAll(() => {
   fsp.unlink(path.join(tmpFilePath, 'ru-hexlet-io-courses.html'));
@@ -30,11 +26,10 @@ afterAll(() => {
 test('Download page', async () => {
   nock(/ru\.hexlet\.io/)
     .get(/\/courses/)
-    .reply(200, actualData);
-  const actual = actualData;
+    .reply(200, data);
+  const actual = data;
   await downloadPage(tmpFilePath, 'https://ru.hexlet.io/courses');
-  const expected = await fsp.readFile(path.join(tmpFilePath, 'ru-hexlet-io-courses.html'));
-  console.log(expected);
+  const expected = await fsp.readFile(path.join(tmpFilePath, 'ru-hexlet-io-courses.html'), 'utf-8');
   expect(expected).toEqual(actual);
 });
 
@@ -42,7 +37,5 @@ test('Path not exist', async () => {
   nock(/ru\.hexlet\.io/)
     .get(/\/courses/)
     .reply(200, 'data');
-  // const expected1 = await downloadPage('blablabla', 'https://ru.hexlet.io/courses');
-  // console.log('EXPECTED 1', expected1);
-  expect(async () => { await downloadPage('blablabla', 'https://ru.hexlet.io/courses'); }).toThrow();
+  expect(async () => { await downloadPage('blablabla', 'https://ru.hexlet.io/courses'); }).rejects.toThrow();
 });
