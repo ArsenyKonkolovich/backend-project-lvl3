5977e3d52ef318fe577e100e618986d934f83822
Arseny <seniaslams@gmail.com>
2022-09-12 15:31:50 +0500

    Update dowload logic

--- src/index.js
+++ src/index.js
@@ -3,31 +3,33 @@ import fsp from 'fs/promises';
 import path from 'path';
 import * as cheerio from 'cheerio';
 
-const loadHtmlPage = (filePath, url) => axios.get(url)
-  .then(({ data }) => fsp.writeFile(`${filePath}.html`, data))
-  .catch((e) => { throw new Error(e); });
+// const loadHtmlPage = (filePath, url) => axios.get(url)
+//   .then(({ data }) => fsp.writeFile(`${filePath}.html`, data))
+//   .catch((e) => { throw new Error(e); });
 
-const loadImage = (filePath, url, fileName) => {
+const loadHtmlPage = (filePath, url, fileName) => {
   const dirName = `${fileName}_files`;
   const dirPath = `${filePath}_files`;
   const htmlFilePath = `${filePath}.html`;
   let imageLinks = [];
-  return fsp.readFile(htmlFilePath)
-    .then((data) => {
-      const $ = cheerio.load(data);
+  let $;
+  return axios.get(url)
+    .then(({ data }) => {
+      $ = cheerio.load(data);
       imageLinks = $('img').toArray();
-    })
-    .catch((e) => { throw new Error(e); })
-    .then(() => {
       imageLinks.forEach((link) => {
-        console.log(link);
-        const imageName = path.parse(link).base;
-        axios.get(`${url}${link}`)
+        console.log($(link).attr('src'));
+        const imageLink = $(link).attr('src');
+        const imageName = path.parse(imageLink).base;
+        axios.get(`${url}${imageLink}`)
+          // eslint-disable-next-line no-shadow
           .then(({ data }) => fsp.writeFile(path.join(dirPath, imageName), data))
           .catch((e) => { throw new Error(e); });
-        link.attr('src', `${dirName}/${imageName}`);
+        $(link).attr('src', `${dirName}/${imageName}`);
       });
-    });
+    })
+    .catch((e) => { throw new Error(e); })
+    .then(() => fsp.writeFile(htmlFilePath, $.html()));
 };
 
 const downloadPage = (filePath, url) => {
@@ -35,14 +37,14 @@ const downloadPage = (filePath, url) => {
   const resultPath = path.join(filePath, fileName);
   return fsp.mkdir(`${resultPath}_files`, { recursive: true })
     .catch((e) => { throw new Error(e); })
-    .then(() => loadHtmlPage(resultPath, url))
-    .catch((e) => { throw new Error(e); })
-    .then(() => loadImage(`${resultPath}`, url, fileName))
+    // .then(() => loadHtmlPage(resultPath, url))
+    // .catch((e) => { throw new Error(e); })
+    .then(() => loadHtmlPage(`${resultPath}`, url, fileName))
     .catch((e) => { throw new Error(e); })
     .then(() => console.log('Done!'));
 };
 
-downloadPage('blabla', 'https://www.google.com');
+// downloadPage('blabla', 'https://www.google.com');
 // const $elements = $(tagName).toArray();
 // $elements.forEach(($element) => $element.attr(attrName, value));
 
