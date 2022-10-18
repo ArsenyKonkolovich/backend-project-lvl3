6cede90725f933a4713f1146f75be541ce9f5380
Arseny <seniaslams@gmail.com>
2022-10-18 12:39:08 +0500

    Update debug

--- src/util.js
+++ src/util.js
@@ -1,7 +1,8 @@
 import path from 'path';
+import debug from 'debug';
 
-// const module = 'page-loader: createFileName';
-// const log = debug(module);
+const module = 'page-loader: normalizeName';
+const log = debug(module);
 
 export const nameChanger = (url) => url.replace(/htt(p|ps):\/\//, '').replace(/\W/g, '-');
 
@@ -9,6 +10,7 @@ export const normalizeName = (url) => {
   const nameForChange = `${path.parse(url.href).dir}/${path.parse(url.href).name}`;
   const nameWhithOutExt = nameChanger(nameForChange);
   const resultName = `${nameWhithOutExt}${path.parse(url.href).ext}`;
+  log(`Filename is ${resultName}`);
   return resultName;
 };
 
