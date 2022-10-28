95df83959c4e46164a93d04206789c8dba0506ed
thanatos <seniaslams@gmail.com>
2022-10-28 16:00:11 +0500

    Add localizeLinks

--- src/util.js
+++ src/util.js
@@ -4,6 +4,30 @@ import debug from 'debug';
 const module = 'page-loader: normalizeName';
 const log = debug(module);
 
+const tags = ['img', 'link', 'script'];
+const tagAttrs = ['src', 'href'];
+
+const mapping = {
+  img: 'src',
+  script: 'src',
+  link: 'href',
+};
+
+const tagNames = Object.keys(mapping);
+
+export const localizeLinks = ($, resourcesToLocalize) => {
+  tags.forEach((tag) => {
+    $(tag).each(function a() {
+      resourcesToLocalize.forEach(([link, filepath]) => {
+        const tagAttr = tagAttrs.filter((el) => $(this).attr(el)).join('');
+        if ($(this).attr(tagAttr) === link) {
+          $(this).attr(tagAttr, filepath);
+        }
+      });
+    });
+  });
+};
+
 export const nameChanger = (url) => url.replace(/htt(p|ps):\/\//, '').replace(/\W/g, '-');
 
 export const normalizeName = (url) => {
@@ -18,4 +42,19 @@ export const isDownloadable = (src, url) => {
   const srcUrl = new URL(src, url);
   const pageUrl = new URL(url);
   return srcUrl.origin === pageUrl.origin;
+};
+
+export const getResoursesLinks = ($, url) => {
+  const links = [];
+  tagNames.forEach((tagName) => {
+    const attrName = mapping[tagName];
+    const srcLinks = $(tagName).toArray();
+    srcLinks.forEach((link) => {
+      const srcLink = $(link).attr(attrName);
+      if (srcLink && isDownloadable(srcLink, url)) {
+        links.push(srcLink);
+      }
+    });
+  });
+  return links;
 };
